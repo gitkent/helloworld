42ae33a9a724572ab3c0f01418d55a7d25e25b38
Kent Lee <kent_lee@bnz.co.nz>
2019-03-18 22:40:46 +1300

    fixed test

--- app.js
+++ app.js
@@ -8,27 +8,23 @@ const fs = require('fs')
 const metadata_file = './package.json'
 
 const o = {}
-var version = 'unknown'
-var description = 'unknown'
 var key = 'helloworld';
+
+var contents = fs.readFileSync("package.json");
+var jsonContent = JSON.parse(contents);
+
+var data = {lastcommitsha : revision, version : jsonContent.version, description: jsonContent.description};
+
 o[key] = [];
-
-try {
-  if (fs.existsSync(metadata_file)) {
-    var contents = fs.readFileSync("package.json");
-    var jsonContent = JSON.parse(contents);
-    version = jsonContent.version;
-    description = jsonContent.description;
-  }
-} catch(waerning) {
-  console.warning(warning)
-}
-
-var data = {lastcommitsha : revision, version : version, description: description};
-
 o[key].push(data);
 
-app.get('/', (req, res) => res.send('Hello World!'))
-app.get('/status', (req, res) => res.send(o))
+app.get('/', function (req, res) {
+  return  res.send('Hello World!');
+});
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.get('/status', function (req, res, next) {
+  return res.json(o);
+});
+
+module.exports = app;
+//app.listen(port, () => console.log(`Example app listening on port ${port}!`))
